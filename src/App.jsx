a2fb17caf73ad07422883ffa8009fbb6327b0e8e
ilonavich <ilonavich@yahoo.de>
2025-01-17 11:23:49 +0100

    first commit and installations

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import HeroSection from "./HeroSection";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

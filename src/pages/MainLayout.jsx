67590a76527cfc33de3ea1f03dc44fea8af51aba
ilonavich <ilonavich@yahoo.de>
2025-01-20 17:20:54 +0100

    MainLayout and Navbar

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,12 +1,14 @@
 // src/components/Layout.jsx
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const Layout = () => {
   return (
     <div>
       <main>
         <Outlet /> {/* Child routes will be rendered here */}
+        <Navbar />
       </main>
     </div>
   );
